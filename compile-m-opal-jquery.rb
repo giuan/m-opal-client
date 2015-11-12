b1db58ac4c5b75d9997ed0c1251aae9d9de16acd
Tiziano Merzi <giuan@iMac-di-Tiziano-Merzi-2.local>
2015-11-12 23:48:20 +0100

    cancellazioni

--- compile-m-opal-jquery.rb
+++ compile-m-opal-jquery.rb
@@ -1,5 +1,3 @@
 require 'opal'
 
 File.open('m-opal-jquery.js', 'wt') { |file| file.puts(Opal.compile(File.read('m-opal-jquery.rb'),{inline_operators: true})) }
-
-system("cp m-opal-jquery.rb /Users/giuan/.rbenv/versions/2.2.1/lib/ruby/gems/2.2.0/gems/opal-0.9.0.beta1/stdlib")
